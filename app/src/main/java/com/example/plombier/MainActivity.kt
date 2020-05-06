9b8e067d8c598dab663fb0413305ddcf4cd47101
aaa1bbb2aaa3bbb4aaa5bbb6 <aaa1bbb2aaa3bbb4aaa5bbb6@gmail.com>
2020-05-06 19:20:19 +0100

    exo2

---
+++ app/src/main/java/com/example/plombier/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.plombier
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            Handler().postDelayed({
+                startActivity(Intent(this, Dashboard::class.java))
+                finish()
+            },500)
+        },1500)
+
+
+
+    }
+}
+
+
